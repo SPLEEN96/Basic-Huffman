15522d5e48a20a108633971cc4a0052864f72091
SPLEEN96 <guay.nicolas1996@gmail.com>
2019-04-08 20:21:46 -0400

    Now decode binary from compressed file

--- Basic-Huffman/Src/HuffmanCoding.h
+++ Basic-Huffman/Src/HuffmanCoding.h
@@ -1,6 +1,8 @@
 #include "HUFFMAN_CORE.h"
 #include "Tree.h"
 
+#include <bitset>
+
 /* Recursive Function that finds all Leaf Nodes and */
 /* Generate a Codeword with the Path taken (Left Branch:0; Right Branch:1)*/
 void GenerateCodewords(const Node *root, std::map<char, std::string> &codewords,
@@ -18,13 +20,11 @@ void GenerateCodewords(const Node *root, std::map<char, std::string> &codewords,
   }
 
   if (!curr_node->lchild && !curr_node->rchild) {
-    std::cout << "\n\n";
-    for (int i = 0; i < len; i++) {
-      std::cout << " " << code[i];
-    }
-    std::cout << "\t" << curr_node->character << ":" << curr_node->frequency;
     code[len] = '\0';
     codewords[curr_node->character] = code;
+
+    std::cout << "\n\n" << codewords[curr_node->character];
+    std::cout << "\t" << curr_node->character << ":" << curr_node->frequency;
   }
 }
 
@@ -36,21 +36,28 @@ void EncodeString(const std::string &input,
   }
 }
 
-void DecodeString(const Node *root, std::string &target) {
+void DecodeBinaryToString(const std::vector<char> &input, const Node *root,
+                          std::string &target) {
   static std::string result = "";
   const Node *initial_root = root;
 
-  for (const char &c : target) {
-    if (c == '0' && root->lchild) {
-      root = root->lchild;
-    }
-    if (c == '1' && root->rchild) {
-      root = root->rchild;
-    }
-    if (!root->lchild && !root->rchild) {
-      result += root->character;
-      root = initial_root;
+  for (const char &c : input) {
+    /* Convert the Char to a Byte */
+    std::string curr_byte = std::bitset<8>(c).to_string();
+    /* Go through each Bits of the Char */
+    for (const char &bit : curr_byte) {
+      if (bit == '0' && root->lchild) {
+        root = root->lchild;
+      }
+      if (bit == '1' && root->rchild) {
+        root = root->rchild;
+      }
+      if (!root->lchild && !root->rchild) {
+        result += root->character;
+        root = initial_root;
+      }
     }
   }
+  result+='\0';
   target = result;
 }
